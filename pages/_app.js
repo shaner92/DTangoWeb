56de8b6e64552921fce548883b41ed0ec779e708
shaner92 <46929382+shaner92@users.noreply.github.com>
2021-10-18 14:23:24 -0400

    work

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,30 @@
 import '../styles/globals.css'
 import Link from 'next/link'
-
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
+import { createCustomTheme } from '../src/theme';
+import useSettings from '../src/hooks/useSettings';
 import Amplify from 'aws-amplify'
 import config from '../src/aws-exports'
+
 Amplify.configure({
   ...config,
   ssr: true
 })
 
 export default function MyApp({ Component, pageProps }) {
+const { settings } = useSettings();
+
+const theme = createCustomTheme({
+  direction: settings.direction,
+  responsiveFontSizes: settings.responsiveFontSizes,
+  roundedCorners: settings.roundedCorners,
+  theme: settings.theme
+});
+
+  
   return (
-    <div>
+    <ThemeProvider theme={theme}>
+            <CssBaseline />
       <nav >
         <Link href="/">
           <span >Home</span>
@@ -20,7 +34,7 @@ export default function MyApp({ Component, pageProps }) {
         </Link>
       </nav>
       <Component {...pageProps} />
-    </div>
+    </ThemeProvider>
   )
 }
 
